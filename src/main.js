db2aaae0471517918e7047262371ccc98de5e942
Olga Garkusha <olga.garkusha.uk@gmail.com>
2022-11-07 15:25:44 +0100

    added debouncing scroll events

--- src/main.js
+++ src/main.js
@@ -6,13 +6,19 @@ import firebase from 'firebase'
 import firebaseConfig from '@/config/firebase'
 import FontAwesome from '@/plugins/fontAwesome'
 import ClickOutsideDirective from '@/plugins/ClickOutsideDirective'
+import PageScrollDirective from '@/plugins/PageScrollDirective'
 
 // Initialize Firebase:
 firebase.initializeApp(firebaseConfig)
 
 const app = createApp(App)
 
-app.use(router).use(store).use(FontAwesome).use(ClickOutsideDirective)
+app
+  .use(router)
+  .use(store)
+  .use(FontAwesome)
+  .use(ClickOutsideDirective)
+  .use(PageScrollDirective)
 
 const requireComponent = require.context('./components', true, /App[A-Z]\w+\.(vue|js)$/)
 requireComponent.keys().forEach(function (fileName) {
